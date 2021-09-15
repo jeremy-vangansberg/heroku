2c41593faa52af31cc3771e8227c5d9aea6846cf
J30V <64211178+J30V@users.noreply.github.com>
2021-09-15 09:18:23 +0200

    maj

--- app.py
+++ app.py
@@ -37,19 +37,19 @@ with info_client :
 amt_inc_total = np.log(df_model.loc[df_model.index == int(id_client), 'AMT_INCOME_TOTAL'].values[0])
 x_a = [np.log(df_model['AMT_INCOME_TOTAL'])]
 fig_a = ff.create_distplot(x_a,['AMT_INCOME_TOTAL'], bin_size=0.3)
-fig_a.add_vline(x=amt_inc_total, annotation_text='Vous etes ici')
+fig_a.add_vline(x=amt_inc_total, annotation_text=' Vous êtes ici')
 
 
 x_b = [np.log(df_model['AMT_CREDIT'])]
 var = np.log(df_model.loc[df_model.index == int(id_client), 'AMT_CREDIT'].values[0])
 fig_b = ff.create_distplot(x_b,['AMT_CREDIT'], bin_size=0.3)
-fig_b.add_vline(x=var, annotation_text='Vous etes ici')
+fig_b.add_vline(x=var, annotation_text=' Vous êtes ici')
 
 
 x_c = [np.log(df_model['SUM(previous.AMT_APPLICATION)']+1)]
 var = np.log(df_model.loc[df_model.index == int(id_client), 'SUM(previous.AMT_APPLICATION)'].values[0])
 fig_c = ff.create_distplot(x_c,['SUM(previous.AMT_APPLICATION)'], bin_size=0.3)
-fig_c.add_vline(x=var, annotation_text='Vous etes ici')
+fig_c.add_vline(x=var, annotation_text=' Vous êtes ici')
 
 
 
@@ -60,34 +60,39 @@ with info_comp :
     st.plotly_chart(fig_c, use_container_width=True)
     
 with result_ml :
-    st.header('''Pourcentage de chance d'acceptation du crédit''')
-    st.write(model.predict_proba(df_model.loc[df_model.index == int(id_client)])[0][1])
+    st.header('''Résultat de la demande de crédit''')
+    per_pos = model.predict_proba(df_model.loc[df_model.index == int(id_client)])[0][1]
+    if per_pos > 0.66 :
+        st.markdown("<p style=color:Green;font-weight:bold> Votre crédit est accepté</p>" , unsafe_allow_html=True)
+    else :
+        st.markdown("<p style=color:Red;font-weight:bold> Votre crédit est refusé</p>" , unsafe_allow_html=True)
+    st.write('Votre crédit est accepté si ce score est supérieur à 0.66 : {}'.format(per_pos))
 
 with feature_imp :
-    st.title('''Variables les plus importantes''')
-    var_pos = ['AMT_CREDIT', 'ORGANIZATION_TYPE',
+    st.title('''Variables les plus importantes dans le calcul de l'allocation''')
+    var_pos = ['AMT_CREDIT',
        'SUM(previous.AMT_CREDIT)', 'SKEW(previous.CNT_PAYMENT)',
        'STD(previous.RATE_DOWN_PAYMENT)']
 
     var_neg = ['SUM(previous.AMT_APPLICATION)', 'SUM(previous.AMT_GOODS_PRICE)',
-       'ORGANIZATION_TYPE',
-       'SKEW(previous.AMT_APPLICATION)', 'SKEW(previous.SELLERPLACE_AREA)']
+        'SKEW(previous.AMT_APPLICATION)', 'SKEW(previous.SELLERPLACE_AREA)']
     
     df_feat_imp = df 
-
+    st.header('Plus ces valeurs sont hautes, plus vous avez de chances que votre crédit soit accepté')
     df_pos = df_feat_imp.loc[df_feat_imp.index == int(id_client)][var_pos].transpose()
-    df_neg = df_feat_imp.loc[df_feat_imp.index == int(id_client)][var_neg].transpose()
     st.write(df_pos)
+    st.header('Plus ces valeurs sont hautes, plus vous avez de chances que votre crédit soit refusé')
+    df_neg = df_feat_imp.loc[df_feat_imp.index == int(id_client)][var_neg].transpose()
     st.write(df_neg)
 
 with filtered_dataset :
     st.title('''Explorateur du jeu de données''')
     is_check = st.checkbox("Affichage des données")
     st.sidebar.header('Explorateur du jeu de données')
-    columns_display = st.sidebar.multiselect("Variables à afficher", df_model.columns)
+    columns_display = st.sidebar.multiselect("Étape n°1 : Sélectionner les variables à afficher", df_model.columns)
     columns_display = list(columns_display)
 
-    columns_filter = st.sidebar.multiselect("Variable à filtrer", df_model.columns)
+    columns_filter = st.sidebar.multiselect("Étape n°2 : Sélectionner les variables à filtrer", columns_display)
     columns_filter = list(columns_filter)
     
     min_max = st.checkbox("Ordre croissant")
