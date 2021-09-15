fe5d7434b1dd382fd03cf88a69b8ebbd1a5af1f8
J30V <64211178+J30V@users.noreply.github.com>
2021-09-15 09:53:10 +0200

    maj

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Import des librairies
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -6,12 +7,16 @@ import seaborn as sns
 import joblib
 import plotly.figure_factory as ff
 
+# Import des données
 df_model = pd.read_csv('csv/data_model_sampled.csv',index_col = 'SK_ID_CURR')
 
+# Modification des données en chaîne de charactères pour la transposition
 df = df_model.astype(np.dtype(str))
 
+# Chargement du modèle
 model = joblib.load('reg_log.sav')
 
+# Instanciation des différentes section
 header = st.container()
 result_ml = st.container()
 info_client = st.container()
@@ -19,21 +24,21 @@ info_comp = st.container()
 feature_imp = st.container()
 filtered_dataset = st.container()
 
+# Section 1 de la sidebar
 st.sidebar.header('Sélection du numéro client')
-
 id_client = st.sidebar.text_input('Identifiant client', value="174545",max_chars=6)
 
+# Affichage du titre et des données relatives au client sélectionné
 with header :
     st.title('''Tableau de bord d'allocation de crédit client''' )
-
 df_client = df.loc[df.index == int(id_client)].transpose()
 df_client.columns = ['Informations clients']
 
-
 with info_client :
-    st.header(''' informations personnelles''')
+    st.header('''Informations personnelles''')
     st.write(df_client)
 
+# Préparation des graphiques comparatifs
 amt_inc_total = np.log(df_model.loc[df_model.index == int(id_client), 'AMT_INCOME_TOTAL'].values[0])
 x_a = [np.log(df_model['AMT_INCOME_TOTAL'])]
 fig_a = ff.create_distplot(x_a,['AMT_INCOME_TOTAL'], bin_size=0.3)
@@ -51,14 +56,17 @@ var = np.log(df_model.loc[df_model.index == int(id_client), 'SUM(previous.AMT_AP
 fig_c = ff.create_distplot(x_c,['SUM(previous.AMT_APPLICATION)'], bin_size=0.3)
 fig_c.add_vline(x=var, annotation_text=' Vous êtes ici')
 
-
-
+# Visualisation des graphiques
 with info_comp :
-    st.header(''' informations comparatives''')
+    st.header('''Informations comparatives''')
+    st.subheader('Vos revenus')
     st.plotly_chart(fig_a, use_container_width=True)
+    st.subheader('Montant du crédit')
     st.plotly_chart(fig_b, use_container_width=True)
+    st.subheader('Somme des crédits demandés lors des précédentes demandes')
     st.plotly_chart(fig_c, use_container_width=True)
-    
+
+# Résultat de la modélisation    
 with result_ml :
     st.header('''Résultat de la demande de crédit''')
     per_pos = model.predict_proba(df_model.loc[df_model.index == int(id_client)])[0][1]
@@ -68,6 +76,7 @@ with result_ml :
         st.markdown("<p style=color:Red;font-weight:bold> Votre crédit est refusé</p>" , unsafe_allow_html=True)
     st.write('Votre crédit est accepté si ce score est supérieur à 0.66 : {}'.format(per_pos))
 
+# Les variables les plus importantes dans la modélisation
 with feature_imp :
     st.title('''Variables les plus importantes dans le calcul de l'allocation''')
     var_pos = ['AMT_CREDIT',
@@ -85,6 +94,7 @@ with feature_imp :
     df_neg = df_feat_imp.loc[df_feat_imp.index == int(id_client)][var_neg].transpose()
     st.write(df_neg)
 
+# Outil permettant de sélectionner des variables et visualiser les données par rapport à plusieurs clients
 with filtered_dataset :
     st.title('''Explorateur du jeu de données''')
     is_check = st.checkbox("Affichage des données")
